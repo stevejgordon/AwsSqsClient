af9314e7046d4dec0ba26dfde237f307c843962d
Steve Gordon <sgordon@hotmail.co.uk>
2019-11-14 07:57:47 +0000

    Optimisations to AccountId

--- src/Aws.Sqs.Core/Primitives/AccountId.cs
+++ src/Aws.Sqs.Core/Primitives/AccountId.cs
@@ -1,42 +1,81 @@
 ﻿using System;
+using System.Globalization;
 
 namespace HighPerfCloud.Aws.Sqs.Core.Primitives
 {
-    public readonly struct AccountId : IEquatable<AccountId>
+    /// <summary>
+    /// Represents an AWS Account Identifier.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The AWS account ID is a 12-digit number, such as 123456789012, that is used to construct Amazon Resource Names (ARNs).
+    /// </para>
+    /// <para>
+    /// Internally, this type uses a long to store the data for the account ID.
+    /// This requires only 64-bits (8-bytes) per account ID vs. 192-bits is using a string, 3x smaller.
+    /// </para>
+    /// </remarks>
+    public readonly struct AccountId :  IComparable<AccountId>, IEquatable<AccountId>
     {
-        public AccountId(string accountId)
+        private readonly long _value;
+
+        private static ReadOnlySpan<char> Format => new[] { '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' };
+
+        public AccountId(long accountId)
         {
-            if (string.IsNullOrWhiteSpace(accountId))
-                throw new ArgumentException(message: "Account ID cannot be null, empty or contain whitespace", nameof(accountId));
-
-            if (accountId.Length != 12)
-                throw new ArgumentException(message: "Account ID must have a length of 12 characters", nameof(accountId));
-
-            foreach (var character in accountId)
+            if (accountId < 0)
             {
-                if (!char.IsDigit(character))
-                    throw new ArgumentException(message: "Account ID may only contain digits", nameof(accountId));
+                throw new ArgumentException(message: "Account ID cannot be created from a negative value", nameof(accountId));
             }
 
-            Value = accountId;
+            if (accountId > 999_999_999_999) // max 12 digits allows for account IDs
+            {
+                throw new ArgumentException(message: "Account ID cannot be created from a value larger than 12 digits in length", nameof(accountId));
+            }
+
+            _value = accountId;
         }
-
-        public string Value { get; }
-
+        
         public static bool operator ==(AccountId left, AccountId right) => Equals(left, right);
 
         public static bool operator !=(AccountId left, AccountId right) => !Equals(left, right);
 
         public override bool Equals(object obj) => (obj is AccountId accountId) && Equals(accountId);
 
-        public bool Equals(AccountId other) => (Value) == (other.Value);
+        public bool Equals(AccountId other) => (_value) == (other._value);
 
-        public override int GetHashCode() => HashCode.Combine(Value);
+        public override int GetHashCode() => HashCode.Combine(_value);
 
-        public static implicit operator string(AccountId accountId) => accountId.Value;
+        public static implicit operator long(AccountId accountId) => accountId._value;
 
-        public static implicit operator AccountId(string accountId) => new AccountId(accountId);
+        public static implicit operator AccountId(long accountId) => new AccountId(accountId);
 
-        public override string ToString() => Value;
+        public int CompareTo(AccountId other) => _value.CompareTo(other._value);
+
+        public static AccountId Default => new AccountId(0);
+
+        /// <summary>
+        ///   Tries to format the value of the current <see cref="AccountId"/> instance into the provided <see cref="Span{T}"/> of characters.
+        /// </summary>
+        /// <param name="destination">When this method returns, this instance's value formatted as a <see cref="Span{T}"/> of characters.</param>
+        /// <returns>
+        ///   <c>true</c> for success.
+        ///   <c>false</c> if destination was too short, or the formatting failed.
+        /// </returns>
+        public bool TryFormat(Span<char> destination)
+        {
+            if (destination.Length < 12)
+                return false; // not enough capacity
+
+            var formatted = _value.TryFormat(destination, out var written, Format, CultureInfo.InvariantCulture);
+
+            return formatted && written == 12;
+        }
+
+        /// <summary>
+        ///   Converts the numeric value of this instance to its equivalent string representation.
+        /// </summary>
+        /// <returns>The string representation of the value of this instance.</returns>
+        public override string ToString() => _value.ToString();
     }
 }
--- test/Aws.Sqs.Client.Tests/Primitives/AccountIdTests.cs
+++ test/Aws.Sqs.Client.Tests/Primitives/AccountIdTests.cs
@@ -1,51 +1,103 @@
 ﻿using HighPerfCloud.Aws.Sqs.Core.Primitives;
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Aws.Sqs.Core.Tests.Primitives
 {
     public class AccountIdTests
     {
-        [Fact]
-        public void Ctor_DoesNotThrowForValidAccountId()
+        [Theory]
+        [InlineData(123456789012)]
+        [InlineData(1)]
+        [InlineData(0012)]
+        public void Ctor_DoesNotThrow_ForValidAccountIds(long value)
         {
-            _ = new AccountId("123456789012");
+            _ = new AccountId(value);
         }
 
         [Fact]
-        public void Ctor_ThrowsForNullAccountId()
+        public void Ctor_Throws_ForNegativeAccountId()
         {
-            _ = Assert.Throws<ArgumentException>(() => new AccountId(null));
+            _ = Assert.Throws<ArgumentException>(() => new AccountId(-1));
         }
 
         [Fact]
-        public void Ctor_ThrowsForEmptyAccountId()
+        public void Ctor_Throws_ForAccountId_GreaterThanTwelveDigits()
         {
-            _ = Assert.Throws<ArgumentException>(() => new AccountId(""));
+            _ = Assert.Throws<ArgumentException>(() => new AccountId(1_000_000_000_000));
+        }
+
+        [Theory]
+        [InlineData(123456789012, new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2' })]
+        [InlineData(12, new[] { '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '2' })]
+        public void TryFormat_Succeeds_AndProducesExpectedCharacters(long acctId, char[] expected)
+        {
+            var accountId = new AccountId(acctId);
+
+            var destination = new char[12].AsSpan();
+
+            var result = accountId.TryFormat(destination);
+
+            result.Should().BeTrue();
+
+            destination.ToArray().Should().BeEquivalentTo(expected);
         }
 
         [Fact]
-        public void Ctor_ThrowsForWhitespaceAccountId()
+        public void TryFormat_Fails_WhenDestinationIsTooSmall()
         {
-            _ = Assert.Throws<ArgumentException>(() => new AccountId("            ")); // 12 empty chars
+            var accountId = new AccountId(123456789012);
+
+            var destination = new char[11].AsSpan();
+
+            var expected = destination.ToArray();
+
+            var result = accountId.TryFormat(destination);
+
+            result.Should().BeFalse();
+
+            destination.ToArray().Should().BeEquivalentTo(expected);
         }
 
         [Fact]
-        public void Ctor_ThrowsForInvalidLengthAccountId_WhenTooShort()
+        public void TwoAccountIds_WithTheSameInternalValue_AreEqual_UsingOperator()
         {
-            _ = Assert.Throws<ArgumentException>(() => new AccountId("12345678901")); // 11 instead of 12
+            var accountId1 = new AccountId(123456789012);
+            var accountId2 = new AccountId(123456789012);
+
+            var result = accountId1 == accountId2;
+
+            result.Should().BeTrue();
         }
 
         [Fact]
-        public void Ctor_ThrowsForInvalidLengthAccountId_WhenTooLong()
+        public void TwoAccountIds_WithTheSameInternalValue_AreEqual()
         {
-            _ = Assert.Throws<ArgumentException>(() => new AccountId("1234567890123")); // 13 instead of 12
+            var accountId1 = new AccountId(123456789012);
+            var accountId2 = new AccountId(123456789012);
+
+            accountId1.Equals(accountId2).Should().BeTrue();
         }
 
         [Fact]
-        public void Ctor_ThrowsForInvalidCharactersInAccountId()
+        public void ImplicitConversionToLong_Succeeds()
         {
-            _ = Assert.Throws<ArgumentException>(() => new AccountId("a23456789012"));
+            var accountId = new AccountId(123456789012);
+
+            long value = accountId;
+
+            value.Should().Be(123456789012);
+        }
+
+        [Fact]
+        public void ImplicitConversionFromLong_Succeeds()
+        {
+            const long accountId = 123456789012;
+
+            AccountId value = accountId;
+
+            value.Should().Be(new AccountId(123456789012));
         }
     }
 }
